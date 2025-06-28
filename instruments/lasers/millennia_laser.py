bd0f24315e177e6ab4ca9f69a11f5546d0116a2c
match@raman <match@Raman>
2025-06-29 02:32:06 +1000

    feat:changing laser timeout to 600 s

--- instruments/lasers/millennia_laser.py
+++ instruments/lasers/millennia_laser.py
@@ -145,7 +145,7 @@ class MillenniaLaser(Laser):
         setpoint = self.get_power_setpoint()
         current_power = self.get_power()
         warmup = self.get_warmup_status()
-        self.watchdog = LaserWatchdog(self.interface, timeout_seconds=60)
+        self.watchdog = LaserWatchdog(self.interface, timeout_seconds=600)
 
         print("Laser initialised.")
         print("Current power setpoint: {}W".format(setpoint))
