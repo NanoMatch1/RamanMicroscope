44fb613ad88419821da981e6be64757c286054c4
NanoMatch <sjbrooke1993@gmail.com>
2025-07-24 18:50:45 +0200

    update

--- instruments/cameras/camera_hardware.py
+++ instruments/cameras/camera_hardware.py
@@ -175,7 +175,7 @@ class RealHardware(CameraHardwareBase):
             self.logger.error(f"TUCAM: Failed to set auto exposure: {ret}")
 
     def set_exposure_time(self, value):
-        # self.close_stream()
+        self.close_stream()
 
         value = float(value) * 1000
         ret1 = TUCAM_Capa_SetValue(self.TUCAMOPEN.hIdxTUCam, TUCAM_IDCAPA.TUIDC_ATEXPOSURE.value, 0)
@@ -189,7 +189,7 @@ class RealHardware(CameraHardwareBase):
         else:
             self.logger.info(f"Exposure time set to {value} ms")
         
-        # self.open_stream()
+        self.open_stream()
 
     def set_image_and_gain(self, img_mode, gain_level):
         ret_set = TUCAM_Capa_SetValue(self.TUCAMOPEN.hIdxTUCam, TUCAM_IDCAPA.TUIDC_IMGMODESELECT.value, img_mode)
